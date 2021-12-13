ce9a8757362a6de9e460c9033179aba82d76c9bf
mansourivali40 <mazrez2340@gmail.com>
2021-12-13 23:49:13 +0330

    ds

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = require('./app');
 
 
 
-  const port= process.env.port || 8000;
+  const port= process.env.PORT || 8000;
   app.listen(port, () => {
     console.log(`App running on port ${port}...`);
   });
