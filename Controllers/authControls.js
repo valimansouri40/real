9b3cc9b306b3e52e2078def2e77297a4b17d0c84
mansourivali40 <mazrez2340@gmail.com>
2021-12-16 15:06:34 +0330

    wert

--- Controllers/authControls.js
+++ Controllers/authControls.js
@@ -17,8 +17,7 @@ const createSendToken = (user, statusCode, res) => {
     expires:new Date(
         Date.now() * 1 + 1000 * 60 * 60 * 24 * 90
     ),
-    http:true,
-    secure = true
+    
 }
 
 
--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express= require('express');
-const path = require('path');
+
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const cors=require("cors")
 const globalerror= require('./Controllers/errorControls');
 const FlowerRoute= require('./Routes/FlowersRoutes');
 const UserRoute= require('./Routes/UserRoutes');
@@ -12,6 +17,22 @@ const AppError=require('./Utils/appError');
 const app= express();
 app.use(express.json());
 
+
+app.use(helmet());
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour!'
+  });
+  app.use('/api', limiter);
+app.use(cookieParser());
+app.use(mongoSanitize());
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+// Data sanitization against XSS
+app.use(xss());
+app.use(cors());
+
 app.use(cookieParser())
 
 app.use((req,res,next)=>{
@@ -19,7 +40,7 @@ app.use((req,res,next)=>{
     
     next();
 })
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/api/v1/flowers',FlowerRoute);
 app.use('/api/v1/users',UserRoute);
